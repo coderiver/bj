9263d4426a16e0ea90687fdbdaf71ad0d699505c
whats0n <mrdvv@mail.ru>
2015-06-09 10:32:29 +0300

    done

--- js/common.js
+++ js/common.js
@@ -14,6 +14,43 @@ head.ready(function() {
 	// $(window).scroll(function(){
 	//     scrollFixedElements()
 	// });
+	
+	//fixed header
+	$(window).scroll(function() {
+		header = $('.header').offset().top;
+		scrollTop = $(window).scrollTop();
+		if (scrollTop > header) {
+			$('.js-fixed').addClass('is-active');
+		}
+		else {
+			$('.js-fixed').removeClass('is-active');
+		};
+	});
+	$(window).load(function() {
+		header = $('.header').offset().top;
+		scrollTop = $(window).scrollTop();
+		if (scrollTop > header) {
+			$('.js-fixed').addClass('is-active');
+		}
+		else {
+			$('.js-fixed').removeClass('is-active');
+		};
+	});
 
-	console.log($('body').html());
+	//nav
+	$('.js-nav-btn').on('click', function() {
+		$('.js-nav').toggleClass('is-active');
+		return false;
+	});
+	$('.js-nav').each(function() {
+		$('body').on('click', function() {
+			$('.js-nav').removeClass('is-active');
+		});
+		$(this).on('click', function(event) {
+			event.stopPropagation();
+		});
+		$('.js-nav a').on('click', function() {
+			$('.js-nav').removeClass('is-active');
+		});
+	});
 });
